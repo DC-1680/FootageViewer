9d22adf660a29ccc6da87207a0781dcf674601e7
DC-1680 <dbconcha@gmail.com>
2018-12-14 18:02:29 -0500

    Add youTube player component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,14 +1,47 @@
-import React from 'react'
+// import React from 'react'
+// import {Player} from './Player';
 
-export const Home = () => {
-	return (
 
-		<div>
-			<h1>
-				'hello test'
-			</h1>
-		</div>
-	)
+// export const Home = () => {
+// 	return (
+
+// 		<div>
+// 			<h1>
+// 				'hello test'
+// 			</h1>
+// 			<Player/>
+// 		</div>
+// 	)
+// }
+
+// export default Home;
+
+import React from 'react';
+import YouTube from 'react-youtube';
+
+class Player extends React.Component {
+	render() {
+		const opts = {
+			height: '390',
+			width: '640',
+			playerVars: { // https://developers.google.com/youtube/player_parameters
+				autoplay: 1
+			}
+		};
+
+		return (
+			<YouTube
+				videoId="2g811Eo7K8U"
+				opts={opts}
+				onReady={this._onReady}
+			/>
+		);
+	}
+
+	_onReady(event) {
+		// access to player in all event handlers via event.target
+		event.target.pauseVideo();
+	}
 }
 
-export default Home;
+export default Player;
